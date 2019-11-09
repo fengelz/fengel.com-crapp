2019491a3cc6361e1c2ae0f2ee085481e3999b2c
fengelz <fengel@gmail.com>
2019-11-09 18:08:36 +0100

    Added index.php for reference

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<html>
+<!--<?php
+        /**
+         * The main template file
+         *
+         * This is the most generic template file in a WordPress theme
+         * and one of the two required files for a theme (the other being style.css).
+         * It is used to display a page when nothing more specific matches a query.
+         * E.g., it puts together the home page when no home.php file exists.
+         *
+         * @link https://codex.wordpress.org/Template_Hierarchy
+         *
+         * @package WordPress
+         * @subpackage Twenty_Seventeen
+         * @since 1.0
+         * @version 1.0
+         */
+      ?>-->
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>
+        <?php bloginfo('name'); ?>
+        <?php wp_title(); ?>
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="manifest" href="site.webmanifest" />
+    <link rel="apple-touch-icon" href="icon.png" />
+    <meta name="google-site-verification" content="M8QltlH4XIp9dUDuBIrhr7KtHka9d0bYFCw7ZUAxx7s" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <!-- inject:css -->
+    <link rel="stylesheet" href="/wp-content/themes/wp-theme-fengelCRApp/assets/css/main.9c63c94f.css">
+    <!-- endinject -->
+    <?php wp_head(); ?>
+</head>
+
+<body><noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <!-- inject:js -->
+    <script src="/wp-content/themes/wp-theme-fengelCRApp/assets/js/main.c0ba7b41.js"></script>
+    <!-- endinject -->
+</body>
+
+</html>
